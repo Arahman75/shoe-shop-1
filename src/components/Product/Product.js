3fac0277c3aa36c9f5d9ad1bed2f92d4a8c2ddb6
Arahmna75 <selfaraman01825@gmail.com>
2022-10-03 22:56:48 +0600

    creat a shoe shop

---
+++ src/components/Product/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+
+const Product = (props) => {
+    const {img, price, ratings, name, seller} = props.product;
+
+    
+    return (
+        <div className='product'>
+           <img src={img} alt="" />
+           <div className='product-info'>
+           <p className='product-name'>{name}</p>
+           <p>Price: {price}</p>
+           <p><small>Seller: {seller}</small></p>
+           <p><small>Ratings: {ratings}</small></p>
+           </div>
+           <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'><p className='cart-text'>Add to Cart</p> <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></button>
+        </div>
+    );
+};
+
+export default Product;
